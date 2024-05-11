1d7e77ddf68a0c46f72f7c3f76e212bb610568ef
Caio Bakerolov <caiobakerolov@icloud.com>
2024-05-11 18:42:51 +1000

    first commit

---
+++ 07 Navegadores antigos/src/main.js
@@ -0,0 +1,8 @@
+// esses 2 imports core e regenerator faz com que o promise rope tambem nos navegadores antigos
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import executa from './modules/promises';
+
+import './assets/css/style.css';
+executa();
